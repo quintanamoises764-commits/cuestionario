5d79d78ccce431ea3562daaad57c9267b0c98c26
quintanamoises764-commits <quintanamoises764@gmail.com>
2026-08-11 09:47:58 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const supabaseClient = supabase.createClient(
+  "https://yougiapspzkllwxhiljb.supabase.co",  // ← URL real de tu proyecto
+  "sb_publishable_yyQdd8r_wgFArLa65DZg_w_7azgkNhf"                        // ← anon public real
+);
