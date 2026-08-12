15de311cfb68d61467489080bef52a444044ea49
quintanamoises764-commits <quintanamoises764@gmail.com>
2026-08-12 08:50:54 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
+
 const supabaseClient = supabase.createClient(
-  "https://oxkzlknclczzvnqnnpdw.supabase.co",  // ← URL real de tu proyecto
-  "sb_publishable_20P3YBj2F_Zc74yaifKrXg_RrCak0oW"                        // ← anon public real
+  "https://oxkzlknclczzvnqnnpdw.supabase.co",  // ← tu URL real
+  "sb_publishable_20P3YBj2F_Zc74yaifKrXg_RrCak0oW",               // ← tu key real
+  {
+    auth: {
+      storage: window.sessionStorage, // la sesión muere al cerrar la pestaña
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
 );
